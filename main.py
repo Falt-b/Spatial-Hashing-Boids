fc334679a12b857868fbd344399ab4e40603cb09
Falt-b <goodoldflats@gmail.com>
2022-04-05 22:51:33 -0400

    Merge branch 'main' of https://github.com/Falt-b/Spatial-Hashing-Boids

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import numpy as np
 from sys import exit
 from random import randint
 
-WIDTH = 400
-HEIGHT = 400
+WIDTH = 600
+HEIGHT = 600
 FRAME_CAP = 60
 BG_COLOR = (20, 20, 20)
 PALLETTE = [
@@ -14,40 +14,55 @@ PALLETTE = [
     (215, 235, 209),
     (177, 216, 207),
 ]
-BOIDS = 70
+BOIDS = 1
 CAPTION = "Boids with pygame"
 ORIGIN = pygame.Vector2(0, 0)
+
+"""angle = ORIGIN.angle_to(self.vel)
+self.image = pygame.transform.rotate(self.original_image, -1 * angle)
+self.rect = self.image.get_rect(center=self.pos)"""
 
 
 class HashMap:
     def __init__(self, container_size: int) -> None:
         self.container_size = container_size
-        self.conversion_factor = 1 / container_size
-        self.width = np.ceil(WIDTH / container_size)
-        self.items = {}
+        self.conversion_factor = 1 / self.container_size
+        self.width = np.ceil(WIDTH / self.container_size)
+        self.hashes = {}
 
     def add_item(self, item: object, hash: int) -> int:
-        if hash in self.items:
-            self.items[hash] = np.append(self.items[hash], item)
-            return self.items[hash].size() - 1
-        self.items.setdefault(hash, np.array([item], object))
+        # checks if key already exists and returns index of element in array
+        if hash in self.hashes:
+            self.hashes[hash] = np.append(self.hashes[hash], item)
+            return self.hashes[hash].size() - 1
+
+        # creates key that contains an array with the item
+        self.hashes.setdefault(hash, np.array([item], object))
         return 0
 
     def remove_item(self, item: object):
-        self.items[item.hash] = np.delete(self.items[item.hash], item.index)
+        # remove element from array by index
+        self.hashes[item.hash] = np.delete(self.hashes[item.hash], item.index)
+
+        # if array has nothing left delete it
+        if self.hashes[item.hash].size() == 0:
+            del self.hashes[item.hash]
+            return
+
+        # otherwise update indexes of all other items
         self.update_indexes(item.hash, item.index, -1)
 
     def update_indexes(self, hash: int, index: int, amount: int):
-        for item in self.items[hash][index + 1 :]:
+        for item in self.hashes[hash][index + 1 :]:
             item.index += amount
 
     def query(self, hash: int):
-        if hash in self.items:
-            return self.items[hash]
+        if hash in self.hashes:
+            return self.hashes[hash]
         return None
 
     def query_item(self, item: object):
-        return np.delete(self.items[item.hash], item.index)
+        return np.delete(self.hashes[item.hash], item.index)
 
     def query_rect(self, max: np.array, min: np.array):
         pass
@@ -57,6 +72,7 @@ class Boid(pygame.sprite.Sprite):
     def __init__(
         self, color: tuple, x: int, y: int, id: int = 0, fill: int = 0
     ) -> None:
+        # initialize sprite and create image
         pygame.sprite.Sprite.__init__(self)
         self.color = color
         self.image = pygame.surface.Surface((24, 10)).convert()
@@ -67,58 +83,29 @@ class Boid(pygame.sprite.Sprite):
             [(0, 1), (6, 5), (15, 0), (24, 5), (15, 10), (6, 5), (0, 9)],
             fill,
         )
+        # create copy of image to be used for rotations
         self.original_image = self.image.copy().convert()
         self.original_image.set_colorkey((0, 0, 0))
-        self.half_width = 12
-        self.half_height = 8
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
 
+        # initialize values for Boid to keep track of
         self.pos = np.array([x, y], float)
         self.vel = np.array([0, 0], float)
         self.accel = np.array([0, 0], float)
-        self.view_range = 100
-        self.hash = 0
-        self.index = 0
         self.id = id
 
+        self.hash_key = 0
+        self.index = 0
+
     def hash(self, table_width: int, container_size: int):
-        return (
+        return int(
             int(self.pos[0] / container_size)
             + int(self.pos[1] / container_size) * table_width
         )
 
-    def get_values(self, boids: np.array):
-        if boids.size() < 1:
-            return 0, 0, 0
-
-        pos = np.array([], float)
-        vel = np.array([], float)
-        dist = np.array([], float)
-        for boid in boids:
-            d = ((self.pos[0] - boid.pos[0]) * (self.pos[0] - boid.pos[0])) + (
-                (self.pos[1] - boid.pos[1]) * (self.pos[1] - boid.pos[1])
-            )
-            if boid != self and d <= 10000:
-                dist = np.append(dist, d)
-                if boid.id == self.id:
-                    pos = np.append(pos, boid.pos)
-                    vel = np.append(vel, boid.vel)
-        smallest_dist = np.argsort(dist)[0]
-        sum_pos = np.sum(np.reshape(pos, (-1, 2)))
-        sum_vel = np.sum(np.reshape(vel, (-1, 2)))
-        return (
-            smallest_dist,
-            np.divide(sum_pos, boids.size()),
-            np.divide(sum_vel, boids.size()),
-        )
-
     def update(self) -> None:
-
-        # rotating and update the image
-        angle = ORIGIN.angle_to(self.vel)
-        self.image = pygame.transform.rotate(self.original_image, angle)
-        self.rect = self.image.get_rect(center=self.pos)
+        pass
 
 
 def main():
@@ -130,11 +117,6 @@ def main():
     dt = 0
     last_frame = 0
     running = True
-
-    b1 = Boid(PALLETTE[0], 200, 200)
-    boids = pygame.sprite.Group()
-    boids.add(b1)
-    a = 0
 
     while running:
         clock.tick(FRAME_CAP)
@@ -148,14 +130,6 @@ def main():
                 if event.key == pygame.K_ESCAPE:
                     running = False
         screen.fill(BG_COLOR)
-
-        if a > 360:
-            a = 0
-        a += 2
-
-        boids.update(a)
-        boids.draw(screen)
-        pygame.draw.circle(screen, (255, 0, 0), b1.pos, 2)
 
         pygame.display.update()
 
